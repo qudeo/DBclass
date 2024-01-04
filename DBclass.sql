e20e7920886f69600756585e211503d99ea96605
qudeo <korapadug@gmail.com>
2024-01-04 15:24:48 +0900

    DBclass

--- DBclass.sql
+++ DBclass.sql
@@ -397,3 +397,193 @@ select * from member8;
 -- auto_increment를 지정하면 값을 따로 주지 않아도 됨
 insert into member8(member_email, member_password) values('aa@aa.com', '1234');
 insert into member8(member_email, member_password) values('bb@bb.com', '1234');
+
+drop table if exists child1;
+create table child1(
+	id bigint primary key,
+    p1 varchar(10),
+    p2 varchar(20)
+    );
+    
+drop table if exists parent1;
+create table parent1(
+	id bigint primary key,
+    p1 varchar(10),
+    p2 varchar(20)
+    );
+    
+drop table if exists child1;
+create table child1(
+	id bigint primary key,
+    c1 varchar(10),
+    c2 varchar(20),
+    p_id bigint, -- 참조할 컬럼
+    -- 외래키 지정 (p_id 컬럼을 parent1 테이블의 id 컬럼을 참조하도록 함)
+    constraint fk_child1 foreign key(p_id) references parent1 (id)
+    );
+    
+    insert into parent1(id, p1, p2) values(1, 'aa', 'aa');
+    insert into parent1(id, p1, p2) values(2, 'bb', 'bb');
+    insert into parent1(id, p1, p2) values(3, 'cc', 'cc');
+    insert into parent1(id, p1, p2) values(4, 'dd', 'dd');
+    select * from parent1;
+    
+    insert into child1 (id, c1, c2, p_id) values (1, 'aaa', 'aaa', 1);
+    -- 부모 id 컬럼에 없는 값을 p_id에 저장
+    insert into child1 (id, c1, c2, p_id) values (2, 'aaa', 'aaa', 2);
+    insert into child1 (id, c1, c2, p_id) values (3, 'aaa', 'aaa', 3);
+    
+    -- 부모테이블의 데이터 삭제
+    -- id=2인 데이터 한 줄을 삭제
+    -- 자식테이블에 id=2인 데이터를 참조하는 부분이 있기 때문에 삭제 불가
+    delete from parent1 where id=2;
+    -- 자식테이블에 id=4인 데이터를 참조하는 부분이 있기 때문에 삭제 가능
+    delete from parent1 where id=4;
+    -- 자식테이블의 부모 id=2를 참조하는 데이터 삭제
+    delete from child1 where id=2;
+    
+    drop table if exists parent2;
+create table parent2(
+	id bigint primary key,
+    p1 varchar(10),
+    p2 varchar(20)
+    );
+    
+drop table if exists child2;
+create table child2(
+	id bigint primary key,
+    c1 varchar(10),
+    c2 varchar(20),
+    p_id bigint, -- 참조할 컬럼
+    -- 외래키 지정 (p_id 컬럼을 parent1 테이블의 id 컬럼을 참조하도록 함)
+    -- 부모데이터 삭제시 자식데이터도 삭제
+    constraint fk_child2 foreign key(p_id) references paren2 (id) on delete cascade
+    );
+    
+    insert into parent2(id, p1, p2) values(1, 'aa', 'aa');
+    insert into parent2(id, p1, p2) values(2, 'bb', 'bb');
+    insert into parent2(id, p1, p2) values(3, 'cc', 'cc');
+    insert into parent2(id, p1, p2) values(4, 'dd', 'dd');
+    select * from parent2;
+    
+    insert into child2 (id, c1, c2, p_id) values (1, 'aaa', 'aaa', 1);
+    insert into child2 (id, c1, c2, p_id) values (2, 'aaa', 'aaa', 2);
+    insert into child2 (id, c1, c2, p_id) values (3, 'aaa', 'aaa', 3);
+    insert into child2 (id, c1, c2, p_id) values (4, 'aaa', 'aaa', 5);
+    select * from child2;
+    
+    delete from parent2 where id=3;
+        
+drop table if exists parent3;
+create table parent3(
+	id bigint primary key,
+    p1 varchar(10),
+    p2 varchar(20)
+    );
+    
+drop table if exists child3;
+create table child3(
+	id bigint primary key,
+    c1 varchar(10),
+    c2 varchar(20),
+    p_id bigint, -- 참조할 컬럼
+    -- 외래키 지정 (p_id 컬럼을 parent1 테이블의 id 컬럼을 참조하도록 함)
+    -- 부모데이터 삭제시 자식데이터는 유지되지만 
+    constraint fk_child3 foreign key(p_id) references parent3 (id) on delete cascade
+    );
+    
+    insert into parent3(id, p1, p2) values(1, 'aa', 'aa');
+    insert into parent3(id, p1, p2) values(2, 'bb', 'bb');
+    insert into parent3(id, p1, p2) values(3, 'cc', 'cc');
+    insert into parent3(id, p1, p2) values(4, 'dd', 'dd');
+    select * from parent3;
+    
+    insert into child3 (id, c1, c2, p_id) values (1, 'aaa', 'aaa', 1);
+    insert into child3 (id, c1, c2, p_id) values (2, 'aaa', 'aaa', 2);
+    insert into child3 (id, c1, c2, p_id) values (3, 'aaa', 'aaa', 3);
+    insert into child3 (id, c1, c2, p_id) values (4, 'aaa', 'aaa', 5);
+    select * from child3;
+    
+    -- 수정쿼리
+update child3 set c1='수정내용' where id=2; -- pk를 기준으로
+update child3 set c1='수정내용', c2='ㅎㅎㅎ' where id=3; -- pk를 기준으로
+
+drop table if exists book;
+create table book(
+id bigint primary key auto_increment,
+b_bookname varchar(20),
+b_publisher varchar(20),
+b_price int
+);
+
+drop table if exists customer;
+create table customer(
+id bigint primary key auto_increment,
+c_name varchar(20),
+c_address varchar(20),
+c_phone varchar(20)
+);
+
+drop table if exists orders;
+create table orders(
+id bigint primary key auto_increment, 
+customer_id bigint,
+book_id bigint,
+o_saleprice int,
+o_orderdate date,
+constraint fk_customer foreign key(customer_id) references customer (id) on delete set null,
+constraint fk_book foreign key(book_id) references book (id) on delete set null
+);
+
+select * from book;
+insert into book (b_bookname, b_publisher, b_price) values ('축구 역사', '좋은출판사', 7000);
+insert into book (b_bookname, b_publisher, b_price) values ('축구 리포트', '나무출판사', 13000);
+insert into book (b_bookname, b_publisher, b_price) values ('축구를 알려주마', '대한출판사', 22000);
+insert into book (b_bookname, b_publisher, b_price) values ('배구의 바이블', '대한출판사', 35000);
+insert into book (b_bookname, b_publisher, b_price) values ('피겨 교과서', '좋은출판사', 8000);
+insert into book (b_bookname, b_publisher, b_price) values ('피칭의 단계별기술', '좋은출판사', 6000);
+insert into book (b_bookname, b_publisher, b_price) values ('야구의 추억 이야기', '나이스미디어', 20000);
+insert into book (b_bookname, b_publisher, b_price) values ('야구 읽어주는 남자', '나이스미디어', 13000);
+insert into book (b_bookname, b_publisher, b_price) values ('올림픽 스토리', '이야기당', 7500);
+insert into book (b_bookname, b_publisher, b_price) values ('olympic history', 'strawberry', 13000);
+
+select * from customer;
+insert into customer (c_name, c_address, c_phone) values ('손흥민', '영국 런던', 000-5000-0001);
+insert into customer (c_name, c_address, c_phone) values ('김연아', '대한민국 서울', 000-6000-0001);
+insert into customer (c_name, c_address, c_phone) values ('김연경', '대한민국 서울', 000-7000-0001);
+insert into customer (c_name, c_address, c_phone) values ('류현진', '캐나다 토론토', 000-8000-0001);
+insert into customer (c_name, c_address) values ('이강인', '프랑스 파리');
+
+select * from orders;
+insert into orders (customer_id, book_id, o_saleprice, o_orderdate) values (1, 1, 6000, '2023-07-01');
+insert into orders (customer_id, book_id, o_saleprice, o_orderdate) values (1, 3, 21000, '2023-07-03');
+insert into orders (customer_id, book_id, o_saleprice, o_orderdate) values (2, 5, 8000, '2023-07-03');
+insert into orders (customer_id, book_id, o_saleprice, o_orderdate) values (3, 6, 6000, '2023-07-04');
+insert into orders (customer_id, book_id, o_saleprice, o_orderdate) values (4, 7, 20000, '2023-07-05');
+insert into orders (customer_id, book_id, o_saleprice, o_orderdate) values (1, 2, 12000, '2023-07-07');
+insert into orders (customer_id, book_id, o_saleprice, o_orderdate) values (4, 8, 13000, '2023-07-07');
+insert into orders (customer_id, book_id, o_saleprice, o_orderdate) values (3, 10, 12000, '2023-07-08');
+insert into orders (customer_id, book_id, o_saleprice, o_orderdate) values (2, 10, 7000, '2023-07-09');
+insert into orders (customer_id, book_id, o_saleprice, o_orderdate) values (3, 8, 13000, '2023-07-10');
+
+-- 1. 모든 도서의 가격과 도서명 조회 
+select b_bookname, b_price from book;
+-- 2. 모든 출판사 이름 조회 
+select b_publisher from book;
+-- 2.1 중복값을 제외한 출판사 이름 조회 
+select distinct b_publisher from book; 
+-- 3. BOOK테이블의 모든 내용 조회 
+select * from book;
+-- 4. 20000원 미만의 도서만 조회 
+select * from book where b_price < 20000;
+-- 5. 10000원 이상 20000원 이하인 도서만 조회
+select * from book where b_price between 10000 and 20000; 
+-- 6. 출판사가 좋은출판사 또는 대한출판사인 도서 조회 
+select * from book where b_publisher='좋은출판사' or b_publisher='대한출판사';
+select * from book where b_publisher in('좋은출판사', '대한출판사');
+-- 7. 도서명에 축구가 포함된 모든 도서를 조회
+select * from book where b_bookname
+-- 8. 도서명의 두번째 글자가 구인 도서 조회
+-- 9. 축구 관련 도서 중 가격이 20000원 이상인 도서 조회
+-- 10. 책 이름순으로 전체 도서 조회
+-- 11. 도서를 가격이 낮은 것 부터 조회하고 같은 가격일 경우 도서명을 가나다 순으로 조회
