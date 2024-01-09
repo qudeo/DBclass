e1aae67dc5f003938aeb439bd499fb113091d4d4
qudeo <korapadug@gmail.com>
2024-01-09 10:40:09 +0900

    투표

--- DBclass.sql
+++ DBclass.sql
@@ -1018,3 +1018,61 @@ insert into tbl_party_202005 values ('P2', 'B정당', '2010-02-01', '명대표',
 insert into tbl_party_202005 values ('P3', 'C정당', '2010-03-01', '기대표', '02', '1111', '0003');
 insert into tbl_party_202005 values ('P4', 'D정당', '2010-04-01', '옥대표', '02', '1111', '0004');
 insert into tbl_party_202005 values ('P5', 'E정당', '2010-05-01', '임대표', '02', '1111', '0005');
+
+-- concat () : 문자열을 이어 붙일 때
+-- substr () : 문자열의 특정 범위를 잘라낼 때
+-- 주민번호 : 2401093123123 
+select m.m_no, m.m_name, p.p_code,
+		case	when m.p_school='1' then '고졸'
+				when m.p_school='2' then '학사'
+                when m.p_school='3' then '석사'
+                when m.p_school='4' then '박사'
+                else '없음'
+		end as p_school,
+		concat(substr(m_jumin, 1, 6), '-', substr(m_jumin, 7, 7)) as '주민번호',
+        concat (p.p_tell, '-', p.p_tel2, '-', p.p_tel3) as '대표전화'
+        from tbl_member_202005 m, tbl_party_202005 p where m.p_code=p.p_code;
+        
+select concat(
+				case when substr(v_jumin, 7, 1) in ('1', '2') then '19'
+					 when substr(v_jumin, 7, 1) in ('3', '4') then '20'
+                end,
+                substr(v_jumin, 1, 2) '년', substr(v_jumin, 3, 2)) '월', substr(v_jumin, 5, 2), '일생')
+	as '생년월일' from tbl_vote_202005;
+    
+    -- 만나이 => 현재년도 - 태어난년도
+    select now() from dual;
+    select 2024-1999 from dual;
+    -- 현재시간에서 년도만
+    select date_format(now(), '%Y') from dual;
+    -- 뺄셈에 활용하기 위해 형변환
+    select cast(date_format(now(), '%Y') as unsigned) from dual;
+  -- 만나이 계산 
+select concat(
+				'만',
+                cast(date_format(now(), '%Y') as unsigned) -
+                concat(
+						case when substr(v_jumin, 7, 1) in ('1', '2') then '19'
+							 when substr(v_jumin, 7, 1) in ('3', '4') then '20'
+							 end,
+                        substr(v_jumin, 1, 2)     
+					  ),
+				'세'
+			 ) as '나이'
+			from tbl_vote_202005;
+-- 성별
+select case when substr(v_jumin, 7, 1) in ('1', '3') then '남'
+			when substr(v_jumin, 7, 1) in ('2', '4') then '여'
+            end as '성별'
+	from tbl_vote_202005;
+    
+-- 투표시간
+select concat(substr(v_time, 1, 2), ':', substr(v_time, 3, 2)) as '투표시간'
+	from tbl_vote_202005;
+    
+-- 유권자 확인
+select
+		case	when v_confirm='Y' then '확인'
+				when v_confirm='N' then '미확인'
+		end as v_confirm
+	from tbl_vote_202005;
